e99a4f4500919d045bad927e4ba7f3ff06f3d604
Brent Pitman <bpitman@netflix.com>
2015-03-20 17:30:41 -0700

    Update versions

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,12 +2,13 @@ import sbt._
 
 object Dependencies {
   object Versions {
-    val scala      = "2.11.5"
-    val slf4j      = "1.7.10"
-    val spectator  = "0.21.0"
+    val scala     = "2.11.5"
+    val slf4j     = "1.7.10"
+    val spectator = "0.21.0"
+    val iep       = "0.1.13"
+    val rxjava    = "1.0.8"
+    val rxnetty   = "0.4.7"
   }
-
-  import Versions._
 
   val archaiusCore    = "com.netflix.archaius" % "archaius-core" % "0.6.5"
   val awsObjectMapper = "com.netflix.awsobjectmapper" % "awsobjectmapper" % "1.9.16.2"
@@ -18,24 +19,24 @@ object Dependencies {
   val jodaTime        = "joda-time" % "joda-time" % "2.5"
   val karyonAdmin     = "com.netflix.karyon2" % "karyon-admin-web" % "2.2.00-ALPHA7"
   val karyonCore      = "com.netflix.karyon2" % "karyon-core" % "2.2.00-ALPHA7"
-  val iepConfig       = "com.netflix.iep" % "iep-config" % "0.1.13"
-  val iepNflxEnv      = "com.netflix.iep" % "iep-nflxenv" % "0.1.13"
-  val iepRxHttp       = "com.netflix.iep-shadow" % "iepshadow-iep-rxhttp" % "0.1.13"
+  val iepConfig       = "com.netflix.iep" % "iep-config" % Versions.iep
+  val iepNflxEnv      = "com.netflix.iep" % "iep-nflxenv" % Versions.iep
+  val iepRxHttp       = "com.netflix.iep-shadow" % "iepshadow-iep-rxhttp" % Versions.iep
   val junit           = "junit" % "junit" % "4.10"
   val junitInterface  = "com.novocode" % "junit-interface" % "0.11"
   val jzlib           = "com.jcraft" % "jzlib" % "1.1.3"
-  val rxjava          = "com.netflix.iep-shadow" % "iep-rxjava" % "1.0.8"
-  val rxnetty         = "com.netflix.iep-shadow" % "iep-rxnetty" % "0.4.6"
-  val rxnettyCtxts    = "com.netflix.iep-shadow" % "iep-rxnetty-contexts" % "0.4.6"
-  val rxnettySpectator= "com.netflix.iep-shadow" % "iep-rxnetty-spectator" % "0.4.6"
-  val scalaLibrary    = "org.scala-lang" % "scala-library" % scala
-  val scalaLibraryAll = "org.scala-lang" % "scala-library-all" % scala
+  val rxjava          = "com.netflix.iep-shadow" % "iepshadow-rxjava" % Versions.rxjava
+  val rxnetty         = "com.netflix.iep-shadow" % "iepshadow-rxnetty" % Versions.rxnetty
+  val rxnettyCtxts    = "com.netflix.iep-shadow" % "iepshadow-rxnetty-contexts" % Versions.rxnetty
+  val rxnettySpectator= "com.netflix.iep-shadow" % "iepshadow-rxnetty-spectator" % Versions.rxnetty
+  val scalaLibrary    = "org.scala-lang" % "scala-library" % Versions.scala
+  val scalaLibraryAll = "org.scala-lang" % "scala-library-all" % Versions.scala
   val scalaLogging    = "com.typesafe.scala-logging" % "scala-logging_2.11" % "3.1.0"
   val scalaParsec     = "org.scala-lang.modules" % "scala-parser-combinators_2.11" % "1.0.2"
-  val scalaReflect    = "org.scala-lang" % "scala-reflect" % scala
+  val scalaReflect    = "org.scala-lang" % "scala-reflect" % Versions.scala
   val scalatest       = "org.scalatest" % "scalatest_2.11" % "2.2.1"
-  val slf4jApi        = "org.slf4j" % "slf4j-api" % slf4j
-  val spectatorApi    = "com.netflix.spectator" % "spectator-api" % spectator
-  val spectatorNflx   = "com.netflix.spectator" % "spectator-nflx-plugin" % spectator
-  val spectatorSandbox= "com.netflix.spectator" % "spectator-ext-sandbox" % spectator
+  val slf4jApi        = "org.slf4j" % "slf4j-api" % Versions.slf4j
+  val spectatorApi    = "com.netflix.spectator" % "spectator-api" % Versions.spectator
+  val spectatorNflx   = "com.netflix.spectator" % "spectator-nflx-plugin" % Versions.spectator
+  val spectatorSandbox= "com.netflix.spectator" % "spectator-ext-sandbox" % Versions.spectator
 }
